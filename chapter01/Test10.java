096650f73f531c137f7717811beed23376f79a70
jjh0428 <92361061+jjh0428@users.noreply.github.com>
2022-01-13 20:50:18 +0900

    Update Test10.java

--- chapter01/Test10.java
+++ chapter01/Test10.java
@@ -15,30 +15,18 @@ public class Test10 {
 		int k = sc.nextInt();
 		
 		int[] nums = new int[n];
-//	/	Integer[] nums = new Integer[];
+//		Integer[] nums = new Integer[];  -> Collections 사용할 경우
 		
 		for(int i=0; i<nums.length; i++) {
 			nums[i] = sc.nextInt();
 		}
 
 		Arrays.sort(nums);
-//	/	Arrays.sort(nums, Collections.reverseOrder());
-		
-//		int temp;
-//		for(int i=0; i<nums.length; i++) {
-//			for(int j=i+1; j<nums.length; j++) {
-//				if(nums[i]<nums[j]) {
-//					temp = nums[i];
-//					nums[i] = nums[j];
-//					nums[j] = temp;
-//				}
-//			}
-//		}
+//		Arrays.sort(nums, Collections.reverseOrder());
 		
 
 		int first = nums[n-1];
 		int secound = nums[n-2];
-//		int cnt = 0;
 		int cnt = (m/(k+1))*k;
 		cnt = cnt + m%(k+1);
 		int result = 0;
@@ -46,15 +34,6 @@ public class Test10 {
 		result += cnt * first;
 		result += (m-cnt) * secound;
 		
-//		for(int j=0; j<m; j++) {
-//			if(cnt<k) {
-//				result += first;
-//				cnt++;
-//			} else {
-//				result += secound;
-//				cnt = 0;
-//			}
-//		}
 		System.out.println(result);
 	}
 
