89db3a636c1d428feeb6a5a9ac4964e2c13f08d6
jjh0428 <92361061+jjh0428@users.noreply.github.com>
2022-01-13 11:29:57 +0900

    Add files via upload

---
+++ Test04.java
@@ -0,0 +1,19 @@
+package com.java.chapter01;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		
+		switch(134354%7) {
+			case 0: System.out.print("Zero"); break;
+			case 1: System.out.print("One"); break;
+			case 2: System.out.print("Two"); break;
+			case 3: System.out.print("Three"); break;
+			case 4: System.out.print("Four"); break;
+			case 5: System.out.print("Five"); break;
+			case 6: System.out.print("Six"); break;
+		}
+
+	}
+
+}
