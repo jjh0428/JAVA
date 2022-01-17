9f19226d5584e9335720b990cafab8704bddfc67
jjh0428 <92361061+jjh0428@users.noreply.github.com>
2022-01-17 12:17:40 +0900

    Add files via upload

---
+++ Ex01.java
@@ -0,0 +1,27 @@
+package com.java.bankAccount;
+
+public class Ex01 {
+
+	int add(int a, int b) {
+		return a+b;
+	}
+	
+	int add(int a, int b, int c) {
+		return a+b+c;
+	}
+	
+	double add(double a, double b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		
+		Ex01 e = new Ex01();
+		
+		System.out.println(e.add(10, 50));
+		System.out.println(e.add(10, 50, 60));
+		System.out.println(e.add(10.0, 15.9));
+
+	}
+
+}
