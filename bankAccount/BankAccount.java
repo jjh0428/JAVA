4be22cfdb5379251cce7c8fc8c629f09da387187
jjh0428 <92361061+jjh0428@users.noreply.github.com>
2022-01-17 11:13:29 +0900

    Add files via upload

--- bankAccount/BankAccount.java
+++ bankAccount/BankAccount.java
@@ -1,44 +1,53 @@
-package project.bankAccount;
+package com.java.bankAccount;
 
 public class BankAccount {
 
-	int balance;
-	Person owner;
+	private int balance;
+	private Person setOwner;
 	
-	// 파라미터: 입금할 액수(정수)
-	// 리턴: 성공여부(불린)
+	public void setBalance(int newBalance) {
+		balance  = newBalance;
+	}
+	public int getValance() {
+		return balance;
+	}
 	
-//	입금
-//	1. 입금을 성공하면 true, 실패하면 false 리턴
-//	2. 입금 성공시, 현재 잔고(balance)에 파라미터로 받은 금액(account)를 더함
-//	3. 입금 성공시, 계좌주의 현금액(cashAcout)를 입금액만큼 줄임
-//	4. 입금할 금액이 음수이면 입금 실패(0원은 성공으로 침)
-//	5. 계좌주의 현금이 입금액보다 작을 경우 입금 실패
-//	6. 입금 실패시 "입금 실패입니다. 잔고: xx원, 현금: xx원"
-//	7. 입금 성공시 "xx원 입금하였습니다. 잔고: xx원, 현금: xx원"
+	public void setOwner(Person newOwner) {
+		setOwner = newOwner;
+	}
+	public Person getOwner() {
+		return setOwner;
+	}
 	
-	boolean deposit(int amount) {
-		if(amount < 0 || owner.cashAmount < amount) {
-			System.out.println("입금 실패입니다. 잔고: "+balance+"원, 현금: "+owner.cashAmount);
+	public boolean deposit(int amount) {
+		if(amount < 0 || setOwner.getCashAmount() < amount) {
+			System.out.println("입금 실패입니다. 잔고: "+balance+"원, 현금: "+setOwner.getCashAmount());
 			return false;
 		}
 		balance += amount;
-		owner.cashAmount -= amount;
+//		owner.cashAmount -= amount;
+//		owner.cashAmount = owner.cashAmount - amount;
+		setOwner.setCashAmount(setOwner.getCashAmount() - amount);
 		
-		System.out.println(amount+"원 입금하였습니다. 잔고: "+balance+"원, 현금: "+owner.cashAmount);
+		System.out.println(amount+"원 입금하였습니다. 잔고: "+balance+"원, 현금: "+setOwner.getCashAmount());
 		return true;
 	}
 	
-	// 파라미터: 출금할 액수(정수)
-	// 리턴: 성공여부(불린)
-//	boolean withdraw(int amount) {
-//		
-//	}
+	public boolean withdraw(int amount) {
+		if(amount < 0 || balance < amount) {
+			System.out.println("출금 실패입니다. 잔고: "+balance+"원, 현금: "+setOwner.getCashAmount());
+			return false;
+		}
+		balance -= amount;
+//		owner.cashAmount += amount;
+		setOwner.setCashAmount(setOwner.getCashAmount() + amount);
+		
+		System.out.println(amount+"원 출금하였습니다. 잔고: "+balance+"원, 현금: "+setOwner.getCashAmount());
+		return true;
+	}
 	
-	// 첫 번째 파라미터: 받는 사람(Person)
-	// 두 번쨰 파라미터: 이체할 금액(정수)
-	// 리턴: 성공여부(불린)
-//	boolean transfer(Person to, int amount) {
-//		
-//	}
+	public boolean transfer(Person to, int amount) {
+		
+		return true;
+	}
 }
--- bankAccount/BankDriver.java
+++ bankAccount/BankDriver.java
@@ -1,26 +1,53 @@
-package project.bankAccount;
+package com.java.bankAccount;
 
 public class BankDriver {
 
 	public static void main(String[] args) {
 		
-		// 사람 선언
-		Person pl = new Person();
-		pl.name = "홍길동";
-		pl.age=35;
-		pl.cashAmount = 30000;
+		Person p1 = new Person();
+		p1.setName("홍길동");
+		p1.setAge(25);
+		p1.setCashAmount(30000);
 		
-		System.out.println(pl.name);
-		System.out.println(pl.age);
-		System.out.println(pl.cashAmount);
+//		System.out.println(p1.getName);
+//		System.out.println(p1.getAge);
+//		System.out.println(p1.CashAmount);
 		
-		// 계좌 생성
-		BankAccount al = new BankAccount();
-		al.balance = 100000;
+		BankAccount a1 = new BankAccount();
+		a1.setBalance(100000);
 		
-		// 두 객체의 관계 설정
-		pl.account = al;
-		al.owner = pl;
+		p1.setAccount(a1);
+		a1.setOwner(p1);
+		
+		// 테스트
+		//System.out.println(p1.getName+"의 계좌 잔액: "+p1.getAccount.balance);
+//		BankDriver 클래스에 아래 인스턴스를 생성
+//		p2(Person) 생성
+//		p2에는 장발장, 23, 100000, a2를 할당
+//		a2(BankAccount) 생성
+//		a2에는 500000, p2
+//		
+//		p2의 계좌인 a2에 다음을 차례로 실행
+//		1. 30,000원 입금
+//		2. 170,000원 출금
+//		3. 620.000원 입금
+//		4. 890,000원 출금
+		
+		Person p2 = new Person();
+		p2.setName("장발장");
+		p2.setAge(23);
+		p2.setCashAmount(100000);
+		
+		BankAccount a2 = new BankAccount();
+		a2.setBalance(500000);
+		
+		p2.setAccount(a2);
+		a2.setOwner(p2);
+		
+		System.out.println(a2.deposit(30000));
+		System.out.println(a2.withdraw(170000));
+		System.out.println(a2.deposit(620000));
+		System.out.println(a2.withdraw(890000));
 
 	}
 
